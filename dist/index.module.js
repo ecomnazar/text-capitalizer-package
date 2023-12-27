2b4a08dbe45e8ae8ac5781d5876f100266c4011e
ecomnazar <ecomnazar.me@gmail.com>
2023-12-27 16:19:00 +0500

    success

---
+++ dist/index.module.js
@@ -0,0 +1,2 @@
+var r=function(r){var t=r.str;return(!0).charAt(0).toUpperCase()+t.slice(1)};export{r as Capitalize};
+//# sourceMappingURL=index.module.js.map
---
+++ dist/index.umd.js
@@ -0,0 +1,2 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e||self).capitalizefirstletterofastringpkg={})}(this,function(e){e.Capitalize=function(e){var t=e.str;return(!0).charAt(0).toUpperCase()+t.slice(1)}});
+//# sourceMappingURL=index.umd.js.map
